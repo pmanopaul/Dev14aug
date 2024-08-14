a1ca4e15f45737268354a295d33f461a8b8f32fc
autblt409 <autblt409@LT409.allianceblr.com>
2024-08-14 13:21:41 +0530

    v1 is updated on 14 aug 2024

---
+++ src/a1/stud.java
@@ -0,0 +1,32 @@
+package a1;
+
+import java.util.Scanner;
+
+public class stud 
+{	int regno;
+	String name, dept;
+	Scanner s;
+	void input()
+	{
+		s=new Scanner(System.in);
+		System.out.println("Enter Regno");
+		regno=s.nextInt();
+		System.out.println("Enter Name");
+		name=s.next();
+		System.out.println("Enter Dept");
+		dept=s.next();
+	}
+	void display()
+	{
+		System.out.println("Regno is:"+regno);
+		System.out.println("Name is:"+name);
+		System.out.println("Department is:"+dept);
+	}
+	public static void main(String[] args) 
+	{
+		stud a=new stud();
+		a.input();
+		a.display();
+
+	}
+}
